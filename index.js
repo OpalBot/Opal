bf36d85ebbb85e1f566f7e2a9559196f48ac6d6d
MininMobile <notminin@gmail.com>
2018-07-06 16:37:08 +0200

    add comments

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+//// import modules
 const discord = require("discord.js");
 const moment = require("moment");
 
+//// initialize
+// discord.js
+const bot = new discord.Client();
+
+// moment
 moment.locale();
 
+//// functions
+// log
 function log(text) {
 	console.log(moment().format('LTS') + ' | ' + text);
 }
