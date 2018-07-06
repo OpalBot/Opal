c7d4c98bc2bc7601d457fb855d46878a473c252c
MininMobile <notminin@gmail.com>
2018-07-06 19:23:46 +0200

    JSDoc Util

--- src/util.js
+++ src/util.js
@@ -1,12 +1,27 @@
+/**
+ * Utilities
+ */
 class Util {
+	/**
+	 * Utilities
+	 * @param {Moment} moment import moment class
+	 */
 	constructor(moment) {
 		this.moment = moment;
 	}
 
+	/**
+	 * Print to console with timestamp
+	 * @param {string} text text to print
+	 */
 	log(text) {
 		console.log(this.moment().format('LTS') + ' | ' + text);
 	}
 
+	/**
+	 * Get keys of object
+	 * @param {object} object object
+	 */
 	keys(object) {
 		return Object.keys(object);
 	}
