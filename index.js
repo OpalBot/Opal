ab07efdf6328a564c96592551c5ef542e7e7f4b1
MininMobile <notminin@gmail.com>
2018-07-06 18:11:59 +0200

    add message event

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //// import modules
 const Discord = require("discord.js");
 const Moment = require("moment");
+const Config = require("./src/config.json");
 const _util = require("./src/util");
 
 //// initialize
@@ -16,7 +17,19 @@ const Util = new _util(Moment);
 //// events
 // bot ready
 Bot.on("ready", () => {
-	Util.log(`Connected to ${Bot.guilds} servers`);
+	Util.log(`Connected to ${Bot.guilds.size} servers`);
+});
+
+// bot command
+Bot.on("message", (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(Config.Prefix)) return;
+
+	let args = message.content.split(" ");
+	let command = args[0].substring(Config.Prefix.length);
+	args.shift();
+
+	Util.log(`${Config.Prefix}${command} FROM ${message.author.username} IN ${message.guild.name} (${message.author.id} SENT IN ${message.guild.id})`);
 });
 
 //// login
